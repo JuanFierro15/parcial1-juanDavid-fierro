e6a51179a8da07a465472a222c36197c27dea6c2
Juan Fierro <juandafica19@gmail.com>
2026-03-10 07:48:08 -0500

    fix: se quita el regex

--- js/script.js
+++ js/script.js
@@ -1,32 +1,11 @@
-let contador
+let contador;
 
 function solicitarServicio() {
+  contador++;
 
-    contador++
-
-    alert("Servicios solicitados: " + contador)
-
+  alert("Servicios solicitados: " + contador);
 }
 
-function registrar() {
-
-    let nombre = document.getElementById("nombre").value
-    let correo = document.getElementById("correo").value
-
-    let regex = /@/
-
-    if (nombre == "") {
-        alert("Nombre obligatorio")
-    }
-
-    if (regex.test = correo) {
-        alert("Correo válido")
-    }
-
-}
-
-document.getElementById("modoBtn").addEventListener("click", function() {
-
-    document.body.classList.toggle("dark")
-
-})
+document.getElementById("modoBtn").addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
